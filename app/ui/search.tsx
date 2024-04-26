a2419e12fb172642f57f0a0e10fc4173748fec69
hunter <nrsofttechnologie@gmail.com>
2024-04-26 20:38:52 +0100

    chap 12 : [200~6. Revalidate and redirect

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,34 +2,42 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useRef } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
+
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-  const {replace} = useRouter();
+  const { replace } = useRouter();
   const pathname = usePathname();
+  const inputRef = useRef<HTMLInputElement | null>(null)
 
-  const handleSearch = (term: string) =>{
-    console.log(`Searching... ${term}`);
-    const params = new URLSearchParams(searchParams); 
-    console.log(term, 'searching term');
+  useEffect(() => {
+    inputRef.current?.focus()
+  })
+  
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page','1');
     if (term) {
-      params.set('query', term)
+      params.set('query', term);
+    } else {
+      params.delete('query');
     }
-    else{
-      params.delete('query')
-    }
-    replace(`${pathname}?${params.toString()}`)
-  }
+    replace(`${pathname}?${params.toString()}`);
+    // inputRef.current?.focus()
+  }, 600);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+        ref={inputRef}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e)=>{
-          handleSearch(e.target.value)
+        onChange={(e) => {
+          handleSearch(e.target.value);
         }}
         defaultValue={searchParams.get('query')?.toString()}
       />
